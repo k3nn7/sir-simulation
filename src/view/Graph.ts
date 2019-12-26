91ebe800980eaa12c8698436e1dff085c345202c
Łukasz Lalik <lukasz.lalik@gmail.com>
2019-12-26 22:14:07 +0100

    Add padding to Graph

--- src/view/Graph.ts
+++ src/view/Graph.ts
@@ -18,11 +18,15 @@ export class Graph extends RenderableObject {
   private susceptible: Array<Point>;
   private infected: Array<Point>;
   private removed: Array<Point>;
+  private paddingX: number;
+  private paddingY: number;
 
   constructor(width: number, height: number) {
     super();
     this.width = width;
     this.height = height;
+    this.paddingX = 28;
+    this.paddingY = 15;
     this.resetData();
   }
 
@@ -52,29 +56,33 @@ export class Graph extends RenderableObject {
   protected draw(ctx: CanvasRenderingContext2D, time: number): void {
     ctx.beginPath();
     ctx.strokeStyle = 'black';
-    ctx.moveTo(10, this.height);
-    ctx.lineTo(10, 0);
-    ctx.lineTo(6, 10);
-    ctx.lineTo(14, 10);
-    ctx.lineTo(10, 0);
+    ctx.moveTo(this.paddingX, this.height);
+    ctx.lineTo(this.paddingX, 0);
+    ctx.lineTo(this.paddingX - 4, 10);
+    ctx.lineTo(this.paddingX + 4, 10);
+    ctx.lineTo(this.paddingX, 0);
     ctx.fill();
-    ctx.moveTo(0, this.height - 10);
-    ctx.lineTo(this.width, this.height - 10);
-    ctx.lineTo(this.width - 10, this.height - 14);
-    ctx.lineTo(this.width - 10, this.height - 6);
-    ctx.lineTo(this.width, this.height - 10);
+    ctx.moveTo(0, this.height - this.paddingY);
+    ctx.lineTo(this.width, this.height - this.paddingY);
+    ctx.lineTo(this.width - 10, this.height - (this.paddingY - 4));
+    ctx.lineTo(this.width - 10, this.height - (this.paddingY + 4));
+    ctx.lineTo(this.width, this.height - this.paddingY);
     ctx.fill();
     ctx.stroke();
 
-    const hStep = (this.width - 10) / this.susceptible.length;
-    const vStep = (this.height - 10) / (this.maxY - this.minY);
+    ctx.textBaseline = 'bottom';
+    ctx.textAlign = 'left';
+    ctx.fillText('(0, 0)', 0, this.height);
+
+    const hStep = (this.width - this.paddingX) / this.susceptible.length;
+    const vStep = (this.height - this.paddingY) / (this.maxY - this.minY);
 
     if (this.susceptible.length) {
       ctx.beginPath();
       ctx.strokeStyle = 'blue';
-      ctx.moveTo(10 + this.susceptible[0].x * hStep, (this.maxY - this.susceptible[0].y) * vStep);
+      ctx.moveTo(this.paddingX + this.susceptible[0].x * hStep, (this.maxY - this.susceptible[0].y) * vStep);
       this.susceptible.forEach(value => {
-        ctx.lineTo(10 + value.x * hStep, (this.maxY - value.y) * vStep);
+        ctx.lineTo(this.paddingX + value.x * hStep, (this.maxY - value.y) * vStep);
       });
       ctx.stroke();
     }
@@ -82,9 +90,9 @@ export class Graph extends RenderableObject {
     if (this.infected.length) {
       ctx.beginPath();
       ctx.strokeStyle = 'green';
-      ctx.moveTo(10 + this.infected[0].x * hStep, (this.maxY - this.infected[0].y) * vStep);
+      ctx.moveTo(this.paddingX + this.infected[0].x * hStep, (this.maxY - this.infected[0].y) * vStep);
       this.infected.forEach(value => {
-        ctx.lineTo(10 + value.x * hStep, (this.maxY - value.y) * vStep);
+        ctx.lineTo(this.paddingX + value.x * hStep, (this.maxY - value.y) * vStep);
       });
       ctx.stroke();
     }
@@ -92,9 +100,9 @@ export class Graph extends RenderableObject {
     if (this.removed.length) {
       ctx.beginPath();
       ctx.strokeStyle = 'red';
-      ctx.moveTo(10 + this.removed[0].x * hStep, (this.maxY - this.removed[0].y) * vStep);
+      ctx.moveTo(this.paddingX + this.removed[0].x * hStep, (this.maxY - this.removed[0].y) * vStep);
       this.removed.forEach(value => {
-        ctx.lineTo(10 + value.x * hStep, (this.maxY - value.y) * vStep);
+        ctx.lineTo(this.paddingX + value.x * hStep, (this.maxY - value.y) * vStep);
       });
       ctx.stroke();
     }
