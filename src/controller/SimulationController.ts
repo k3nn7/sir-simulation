2a1660575701734f266468f929c1e0fad91f7088
Łukasz Lalik <lukasz.lalik@gmail.com>
2019-12-26 15:51:40 +0100

    Add inputs for configuring simulation

--- src/controller/SimulationController.ts
+++ src/controller/SimulationController.ts
@@ -3,26 +3,44 @@ import {SIRSimulation, State} from "../model/sir/SIRSimulation";
 
 export class SimulationController {
   private view: Simulation;
+  private horizontalCellsInput: HTMLInputElement;
+  private verticalCellsInput: HTMLInputElement;
+  private infectedCellsInput: HTMLInputElement;
+  private cInput: HTMLInputElement;
 
-  constructor(view: Simulation) {
+  constructor(
+    view: Simulation,
+    horizontalCellsInput: HTMLInputElement,
+    verticalCellsInput: HTMLInputElement,
+    infectedCellsInput: HTMLInputElement,
+    cInput: HTMLInputElement,
+  ) {
+    this.horizontalCellsInput = horizontalCellsInput;
+    this.verticalCellsInput = verticalCellsInput;
+    this.infectedCellsInput = infectedCellsInput;
+    this.cInput = cInput;
     this.view = view;
   }
 
   public async start(): Promise<void> {
-    this.view.setSize(200, 200);
-    this.view.reset();
-    const simulation = new SIRSimulation(200, 200, 0.17);
+    const hCells = parseInt(this.horizontalCellsInput.value, 10);
+    const vCells = parseInt(this.verticalCellsInput.value, 10);
+    const infectedCells = parseInt(this.infectedCellsInput.value, 10);
+    const c = parseFloat(this.cInput.value);
 
-    for (let i = 0; i < 10; i++) {
-      const x = Math.floor(Math.random() * 200),
-        y = Math.floor(Math.random() * 200);
-      console.log(x, y);
+    this.view.setSize(hCells, vCells);
+    this.view.reset();
+    const simulation = new SIRSimulation(hCells, vCells, c);
+
+    for (let i = 0; i < infectedCells; i++) {
+      const x = Math.floor(Math.random() * hCells),
+        y = Math.floor(Math.random() * vCells);
       simulation.infectCell(x, y);
       this.view.infect(x, y);
     }
 
     while (true) {
-      await new Promise(r => setTimeout(r, 10));
+      await new Promise(r => setTimeout(r, 100));
       simulation.epoch();
 
       simulation.lastEvents().forEach((event) => {
@@ -35,8 +53,6 @@ export class SimulationController {
             break;
         }
       });
-
-
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,21 @@ import {SimulationController} from "./controller/SimulationController";
 
 const canvas = <HTMLCanvasElement>document.getElementById('simulation');
 const renderer = new Renderer(canvas.getContext('2d'));
+
 const startSimulationButton = <HTMLButtonElement>document.getElementById('start-simulation');
+const horizontalCellsInput = <HTMLInputElement>document.getElementById('input-horizontal-cells');
+const verticalCellsInput = <HTMLInputElement>document.getElementById('input-vertical-cells');
+const infectedCellsInput = <HTMLInputElement>document.getElementById('input-infected-cells');
+const cInput = <HTMLInputElement>document.getElementById('input-c');
 
 const view = new Simulation(canvas.width, canvas.height);
-const controller = new SimulationController(view);
+const controller = new SimulationController(
+  view,
+  horizontalCellsInput,
+  verticalCellsInput,
+  infectedCellsInput,
+  cInput,
+);
 
 renderer.addObject(view);
 
