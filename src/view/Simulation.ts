f0ded1f786ccc4ac5a8f0f480edb12ac8c1a8d4c
Łukasz Lalik <lukasz.lalik@gmail.com>
2019-12-30 19:59:46 +0100

    View rendering optimization

--- src/view/Simulation.ts
+++ src/view/Simulation.ts
@@ -1,17 +1,33 @@
 import {RenderableObject} from "../engine/RenderableObject";
 import {State} from "../model/sir/SIRSimulation";
 
+class Cell {
+  public x: number;
+  public y: number;
+  public state: State;
+
+  constructor(x: number, y: number, state: State) {
+    this.x = x;
+    this.y = y;
+    this.state = state;
+  }
+}
+
 export class Simulation extends RenderableObject {
-  private cells: Array<Array<State>>;
+  private cells: Array<Array<Cell>>;
+  private susceptible: Set<Cell>;
+  private infected: Set<Cell>;
+  private removed: Set<Cell>;
   private cellsX: number;
   private cellsY: number;
-  private width: number;
-  private height: number;
+  private readonly width: number;
+  private readonly height: number;
 
   constructor(width: number, height: number) {
     super();
     this.width = width;
     this.height = height;
+    this.reset();
   }
 
   public setSize(cellsX: number, cellsY: number): void {
@@ -20,44 +36,48 @@ export class Simulation extends RenderableObject {
   }
 
   public reset(): void {
-    this.cells = new Array<Array<State>>();
+    this.cells = new Array<Array<Cell>>();
+    this.susceptible = new Set<Cell>();
+    this.infected = new Set<Cell>();
+    this.removed = new Set<Cell>();
 
     for (let i = 0; i < this.cellsX; i++) {
-      this.cells[i] = new Array<State>();
+      this.cells[i] = new Array<Cell>();
       for (let j = 0; j < this.cellsY; j++) {
-        this.cells[i][j] = State.Susceptible;
+        const cell = new Cell(i, j, State.Susceptible);
+        this.cells[i][j] = cell;
+        this.susceptible.add(cell);
       }
     }
   }
 
   public infect(x: number, y: number): void {
-    this.cells[x][y] = State.Infected;
+    this.cells[x][y].state = State.Infected;
+    this.susceptible.delete(this.cells[x][y]);
+    this.infected.add(this.cells[x][y]);
   }
 
   public remove(x: number, y: number): void {
-    this.cells[x][y] = State.Removed;
+    this.cells[x][y].state = State.Removed;
+    this.infected.delete(this.cells[x][y]);
+    this.removed.add(this.cells[x][y]);
   }
 
   protected draw(ctx: CanvasRenderingContext2D, time: number): void {
     const cellWidth = this.width / this.cellsX,
       cellHeight = this.height / this.cellsY;
 
-    for (let i = 0; i < this.cellsX; i++) {
-      for (let j = 0; j < this.cellsY; j++) {
-        switch (this.cells[i][j]) {
-          case State.Susceptible:
-            ctx.fillStyle = 'blue';
-            break;
-          case State.Infected:
-            ctx.fillStyle = 'green';
-            break;
-          case State.Removed:
-            ctx.fillStyle = 'red';
-            break;
-        }
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, this.width, this.height);
 
-        ctx.fillRect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
-      }
-    }
+    ctx.fillStyle = 'green';
+    this.infected.forEach(value => {
+      ctx.fillRect(value.x * cellWidth, value.y * cellHeight, cellWidth, cellHeight);
+    });
+
+    ctx.fillStyle = 'red';
+    this.removed.forEach(value => {
+      ctx.fillRect(value.x * cellWidth, value.y * cellHeight, cellWidth, cellHeight);
+    });
   }
 }
